900020dfa9e075d9e221c67ae6462b7680d4d732
howard <liuhoward09@gmail.com>
2017-05-25 11:33:26 +0800

    DataProcess: add formatTickAB(), modify GetTick() for tempory

--- src/DataProcess.cpp
+++ src/DataProcess.cpp
@@ -23,6 +23,7 @@
 #include <hash_set>
 
 #include <boost/date_time.hpp>
+//#include <boost/format.hpp>
 
 #define ELEMENT_COUNT(arr) (sizeof(arr)/sizeof(arr[0]))
 
@@ -172,77 +173,167 @@ int main(int argc, char* argv[])
 
 }
 
-bool a_less_b(const TDBDefine_Tick& a,const TDBDefine_Tick& b)
+bool a_less_b(const TDBDefine_TickAB& a, const TDBDefine_TickAB& b)
 {
     return a.nTime < b.nTime;
 }
 
-void GetTick(THANDLE hTdb, const std::string& strCode, bool bWithAB, int nStartDay, int nEndDay, int nStartTime/* =0*/, int nEndTime/* = 0*/)
+string formatTickAB(TDBDefine_TickAB& tdbTick)
+{
+    stringstream ss;
+    ss << tdbTick.chWindCode << ","
+        << tdbTick.nDate << ","
+        << tdbTick.nTime << ","
+        << tdbTick.nPrice << ","
+        << tdbTick.iVolume << ","
+        << tdbTick.iTurover << ","
+        << tdbTick.nMatchItems << ","
+        << tdbTick.nInterest << ","
+        << tdbTick.chTradeFlag << ","
+        << tdbTick.chBSFlag << ","
+        << tdbTick.iAccVolume << ","
+        << tdbTick.iAccTurover << ","
+        << tdbTick.nHigh << ","
+        << tdbTick.nLow << ","
+        << tdbTick.nOpen << ","
+        << tdbTick.nPreClose << ",";
+
+    int size = ELEMENT_COUNT(tdbTick.nAskPrice);
+    for(int i = 0; i < size; i++) {
+        ss << tdbTick.nAskPrice[i] << ",";
+    }
+    for(int i = 0; i < size; i++) {
+        ss << tdbTick.nBidPrice[i] << ",";
+    }
+    for(int i = 0; i < size; i++) {
+        ss << tdbTick.nAskVolume[i] << ",";
+    }
+    for(int i = 0; i < size; i++) {
+        ss << tdbTick.nBidVolume[i] << ",";
+    }
+
+    ss << tdbTick.nAskAvPrice << ","
+        << tdbTick.nBidAvPrice << ""
+        << tdbTick.iTotalAskVolume << ","
+        << tdbTick.iTotalBidVolume << ","
+        << tdbTick.nIndex << ","
+        << tdbTick.nStocks << ","
+        << tdbTick.nUps << ","
+        << tdbTick.nDowns << ","
+        << tdbTick.nHoldLines << "\n";
+
+    return ss.str();
+}
+
+int timeToindex(int nTime) {
+    int index = 0;
+    int base = 93001;
+    int delta = 0;
+    if(nTime > 130000000) {
+        base = 130001;
+        delta = 7200;
+    }
+
+    int tmp = (nTime / 1000 - base);
+    index += tmp / 10000 * 3600;
+    tmp %= 10000;
+    index += tmp / 100 * 60;
+    index += tmp % 100 + delta; 
+
+    return index;
+}
+
+int GetTickAB(THANDLE hTdb, const std::string& strCode, int nStartDay, int nEndDay, int nStartTime/* =0*/, int nEndTime/* = 0*/, ofstream origin_out, ofstream ofile)
 {
     TDBDefine_ReqTick reqTick = {"",nStartDay, nEndDay, nStartTime, nEndTime};
     strncpy(reqTick.chCode, strCode.c_str(), sizeof(reqTick.chCode));
 
-    if (!bWithAB)
-    {
-        TDBDefine_Tick* pTick = NULL;
-        int nCount;
-        int nRet = TDB_GetTick(hTdb, &reqTick, &pTick, &nCount);
+    TDBDefine_TickAB *pTick = NULL;
+    int nCount;
+    int nRet = TDB_GetTickAB(hTdb, &reqTick, &pTick, &nCount);
 
-        Print("---------------------receive %d records, error code:%d -----------------\n", nCount, nRet);
-        for (int i=0; i<nCount && i<10; i++)
-        {
-            TDBDefine_Tick& tdbTick = *(pTick+i);
-            Print("code:%s, date:%d, time:%d, nprice:%d, vol:%I64d, turover:%I64d, acc vol:%I64d, acc turover:%I64d\n", tdbTick.chWindCode, tdbTick.nDate, tdbTick.nTime, tdbTick.nPrice, tdbTick.iVolume, tdbTick.iTurover, tdbTick.iAccVolume, tdbTick.iAccTurover);
-        }
-        delete [] pTick;
+    if(nRet != 0) {
+        nRet = TDB_GetTickAB(hTdb, &reqTick, &pTick, &nCount);
     }
-    else
-    {
-        
-        TDBDefine_TickAB* pTick = NULL;
-        int nCount;
-        int nRet = TDB_GetTickAB(hTdb, &reqTick, &pTick, &nCount);
 
-        Print("---------------------receive %d records, error code:%d -----------------\n", nCount, nRet);
-        for (int i=0; i<nCount  && i<10; i++)
-        {
-            TDBDefine_TickAB& tdbTick = *(pTick+i);
-            //if(20130409 != tdbTick.nDate)
-#ifdef __PLATFORM_WINDOWS__
-            Print("code:%s, date:%d, time:%d, nprice:%d, vol:%I64d, turover:%I64d, acc vol:%I64d, acc turover:%I64d\n", tdbTick.chWindCode, tdbTick.nDate, tdbTick.nTime, tdbTick.nPrice, tdbTick.iVolume, tdbTick.iTurover, tdbTick.iAccVolume, tdbTick.iAccTurover);
-#else
-            Print("code:%s, date:%d, time:%d, nprice:%d, vol:%lld, turover:%lld, acc vol:%lld, acc turover:%lld\n", tdbTick.chWindCode, tdbTick.nDate, tdbTick.nTime, tdbTick.nPrice, tdbTick.iVolume, tdbTick.iTurover, tdbTick.iAccVolume, tdbTick.iAccTurover);
-#endif
-            
+    if(nRet != 0) {
+        return -1;
+    }
+
+    Print("---------------------receive %d records, error code:%d -----------------\n", nCount, nRet);
+
+    if(nCount <= 0) {
+        return 0;
+    }
+
+    vector<TDBDefine_TickAB> origin_data(nCount);
+    for (int i = 0; i < nCount; i++) {
+        TDBDefine_TickAB &tdbTick = *(pTick + i);
+
+        string record = formatTickAB(tdbTick);
+        origin_out.write(record.c_str(), sizeof(record.c_str()));
+
+        // filter ticks with wrong date or wrong price or wrong time
+        tdbTick.nTime = tdbTick.nTime / 1000 * 1000;
+        if(tdbTick.nDate != nStartDay || tdbTick.nPrice <= 0 || tdbTick.nTime < 93000000 || (113000000 < tdbTick.nTime && tdbTick.nTime < 130000000) || tdbTick.nTime > 150000000) {
+            continue;
         }
-        delete [] pTick;
+        origin_data.push_back(tdbTick);
+    }
+
+    // sort the ticks
+    sort(origin_data.begin(), origin_data.end(), a_less_b);
+
+    vector<TDBDefine_TickAB>::iterator curr = origin_data.begin();
+    vector<TDBDefine_TickAB>::iterator next = origin_data.begin();
+    int currIndex = timeToindex(curr->nTime);
+    int nextIndex;
+
+    // remove duplicates with the same HHMMSS
+    while(next != origin_data.end() && next->nTime == curr->nTime) {
+        next++;
     }
     
-}
-
-void GetK(THANDLE hTdb, const std::string& strCode, CYCTYPE nCycType, int nCycDef, REFILLFLAG nFlag,  int nAutoComplete, int nStartDay, int nEndDay, int nStartTime/*=0*/, int nEndTime/* = 0*/)
-{
-    TDBDefine_ReqKLine reqK = {"", nFlag, 0, 0, nCycType, nCycDef, nAutoComplete, nStartDay, nEndDay, nStartTime, nEndTime};
-    strncpy(reqK.chCode, strCode.c_str(), sizeof(reqK.chCode));
-    TDBDefine_KLine* pKLine = NULL;
-    int nCount =0;
-    //TICK_BEGIN(minitue);1
-    int nRet = TDB_GetKLine(hTdb, &reqK, &pKLine, &nCount);
-    //TICK_END(minitue);
-    Print("---------------------receive %d records, error code:%d -----------------\n", nCount, nRet);
-    for (int i=0; i<nCount && i<10; i++)
-    {
-        TDBDefine_KLine& tdbK = *(pKLine+i);
-#ifdef __PLATFORM_WINDOWS__
-        Print("code:%s, date:%d, time:%d, open:%d, high:%d, low:%d, close:%d, volume:%I64d, turover:%I64d\n", tdbK.chWindCode, tdbK.nDate, tdbK.nTime, tdbK.nOpen, tdbK.nHigh, tdbK.nLow, tdbK.nClose, tdbK.iVolume, tdbK.iTurover);
-#else
-        Print("code:%s, date:%d, time:%d, open:%d, high:%d, low:%d, close:%d, volume:%lld, turover:%lld\n", tdbK.chWindCode, tdbK.nDate, tdbK.nTime, tdbK.nOpen, tdbK.nHigh, tdbK.nLow, tdbK.nClose, tdbK.iVolume, tdbK.iTurover);
-#endif
-        
+    // total number of indice
+    int total = 60 * 60 * 4 - 2;
+    // only one valid tick
+    if(next == origin_data.end()) {
+        nextIndex = total;
     }
-    delete[] pKLine;
-}
+    else {
+        nextIndex = timeToindex(next->nTime);
+    }
 
+    int index = 0;
+    while(true) {
+        while(index < nextIndex) {
+            string record = formatTickAB(*curr);
+            ofile.write(record.c_str(), sizeof(record.c_str()));
+            index++;
+        }
+        if(nextIndex == total) {
+            break;
+        }
+        currIndex = nextIndex;
+        curr = next;
+        // remove duplicates with the same HHMMSS
+        while (next != origin_data.end() && next->nTime == curr->nTime) {
+            next++;
+        }
+
+        if (next == origin_data.end()) {
+            nextIndex = total;
+        } else {
+            nextIndex = timeToindex(next->nTime);
+        }
+   
+    }
+
+    delete[] pTick;
+
+    return 0;
+    
+}
 
 int Print(const char* szFormat, ...)
 {
@@ -286,5 +377,7 @@ vector<string> importStockCode(string srcFile)
         }
     }
     
+    infile.close();
+
     return stockCodes;
 }
